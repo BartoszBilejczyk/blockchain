590c0c5759a9589f454998eb8837e2eb16a260d8
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-05-07 14:04:59 +0200

    deploy and send contract

--- first-contract/test/Inbox.test.js
+++ first-contract/test/Inbox.test.js
@@ -5,53 +5,36 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+const { interface, bytecode } = require('../compile')
 
+let accounts;
+let inbox;
 
+beforeEach(async () => {
+  // Get a list of all accounts
 
+  accounts = await web3.eth.getAccounts();
 
+  // web3.eth.getAccounts()
+  //   .then(fetchedAccounts => {
+  //     console.log(fetchedAccounts)
+  //   })
 
+  // Use one of those account to deploy the contract
 
+  inbox = await new web3.eth.Contract(JSON.parse(interface)) // ABI - interface
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({
+      from: accounts[0],
+      gas: '1000000'
+    })
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Car {
-//   park() {
-//     return 'stopped';
-//   }
-//
-//   drive() {
-//     return 'vroom'
-//   }
-// }
-
-//
-// let car;
-//
-// beforeEach(() => {
-//   car = new Car();
-// })
-//
-// describe('Car Class', () => {
-//   it('can park', () => {
-//     assert.equal(car.park(), 'stopped');
-//   })
-//
-//   it('can drive', () => {
-//     assert.equal(car.drive(), 'vroom');
-//   })
-// })
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(inbox);
+  })
+});
