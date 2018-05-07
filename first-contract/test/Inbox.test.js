66295d4cdc4ce74f15becbdad219efce8f4dfe03
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-05-07 13:05:31 +0200

    Wrote some Mocha tests

---
+++ first-contract/test/Inbox.test.js
@@ -0,0 +1,57 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+
+// Web3 constructor
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+//
+//   drive() {
+//     return 'vroom'
+//   }
+// }
+
+//
+// let car;
+//
+// beforeEach(() => {
+//   car = new Car();
+// })
+//
+// describe('Car Class', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   })
+//
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   })
+// })
