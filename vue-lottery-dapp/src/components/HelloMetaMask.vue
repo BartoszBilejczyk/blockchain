1f97d45de346f92b4ec6ceede24dced842ee49bf
BartoszBilejczyk <bartoszbilejczyk@gmail.com>
2018-07-09 10:55:23 +0200

    Changed the whole HelloMetaMask.vue using mapState and NETWORKS constant and showing styling for metamask available/unavailable

--- vue-lottery-dapp/src/components/HelloMetaMask.vue
+++ vue-lottery-dapp/src/components/HelloMetaMask.vue
@@ -1,23 +1,42 @@
 <template lang="html">
-  <div class="metamask-info">
-    <p>Metamask: {{ web3.injectedWeb3 }}</p>
-    <p>Network: {{ web3.networkId }}</p>
-    <p>Account: {{ web3.coinbase }}</p>
-    <p>Balance: {{ web3.balance }}</p>
+  <div class='metamask-info'>
+    <p v-if="injectedWeb3" id="has-metamask"><i aria-hidden="true" class="fa fa-check"></i> Metamask installed</p>
+    <p v-else id="no-metamask"><i aria-hidden="true" class="fa fa-times"></i> Metamask not found</p>
+    <p>Network: {{ network }}</p>
+    <p>Account: {{ coinbase }}</p>
+    <p>Balance: {{ balance }} Wei // {{ ethBalance }} Eth</p>
   </div>
 </template>
 
 <script>
+import { NETWORKS } from '../utils/constants/networks'
+import { mapState } from 'vuex'
+
 export default {
   name: 'hello-metamask',
-  computed: {
-    web3() {
-      return this.$store.state.web3;
+  computed: mapState({
+    injectedWeb3: state => state.web3.injectedWeb3,
+    network: state => NETWORKS[state.web3.networkId],
+    coinbase: state => state.web3.coinbase,
+    balance: state => state.web3.balance,
+    ethBalance: state => {
+      if (state.web3.web3Instance !== null) return state.web3.web3Instance().fromWei(state.web3.balance, 'ether')
     }
-  }
+  })
 }
 </script>
 
 <style lang="scss" scoped>
 
+.metamask-info {
+  text-align: center;
+}
+
+#has-metamask {
+  color: green;
+}
+
+#no-metamask {
+  color: red;
+}
 </style>
